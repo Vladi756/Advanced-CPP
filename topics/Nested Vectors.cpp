bbbea1287131b933658475f42f2be5e22a5952cd
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-03-31 19:16:21 +0200

    Nested Vectors
    An example of initializing, and iterating through nested vectors. Similar to multi-dimensional arrays.

--- topics/Nested Vectors.cpp
+++ topics/Nested Vectors.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-    std::vector<std::vector<int>> grades = {{1,2,3},{4,5,6},{7,8,9}};
+    std::vector<std::vector<int>> grades = {{1,2,3},{4,5,6},{7,8,9}};   // Unlike n-D arrays, size doesn't need to be specified as vectors know their size cause they aren't stupid.
 
     for(int i = 0; i < 3; i++){
         for(int k = 0; k < 3; k++){
