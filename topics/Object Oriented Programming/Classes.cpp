40976a40ebbee609378dcbe9916881e06beeb328
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-04-03 00:40:15 +0200

    Classes
    Classes are one of the most important concepts in all of Object Oriented Programming.

--- topics/Object Oriented Programming/Classes.cpp
+++ topics/Object Oriented Programming/Classes.cpp
@@ -13,13 +13,13 @@ class User {
 
 int main(){
 
-    User user;                  // Creates new instance of user class - this is a user Object.
+    User user;                      // Creates new instance of user class - this is a user Object.
     user.firstName = "John";
     user.lastName = "Doe";
 
-    std::cout << "First Name: " << user.firstName << std::endl; // Accessible because public;
+    std::cout << "First Name: " << user.firstName << std::endl;     // Accessible because public;
     std::cout << "Last Name: " << user.lastName << std::endl;
-    std::cout << "Rank: " << user.getRank() << std::endl; // Ranking is private, but can be accessed indirectly from public method.
+    std::cout << "Rank: " << user.getRank() << std::endl;           // Ranking is private, but can be accessed indirectly from public method.
 
     return 0;
 }
