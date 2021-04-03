68b7b8211a8fc0faa562d257fb9b876860028db1
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-04-04 00:17:07 +0200

    Friend Functions
    Defined outside of class but can access private members of that class.

---
+++ topics/Object Oriented Programming/Friend Functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+class User {
+    std::string ranking = "Gold";   // Private
+
+
+    public:
+        std::string firstName;
+        std::string lastName;
+  
+        friend void outputStatus(User user); // Declared but not defined in the class.
+};
+
+void outputStatus(User user){
+    std::cout << user.ranking;
+}
+
+int main(){
+
+    User user, user1;
+    user.firstName = "Naomi";
+    user.lastName = "Bell";
+    user.setRank("Gold");
+
+    outputStatus(user); // Not a part of the User class, so no need to prefix with User.outputStatus();
+
+    return 0;
+}
