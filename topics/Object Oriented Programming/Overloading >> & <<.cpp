4c929078302855396274976a742c19833ed49eec
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-04-03 21:28:42 +0200

    Overloading >> & <<
    A little bit more of a complex example demonstrating how yo change the functionality of the >> and << operators to match our needs.

--- topics/Object Oriented Programming/Overloading >> & <<.cpp
+++ topics/Object Oriented Programming/Overloading >> & <<.cpp
@@ -22,12 +22,12 @@ class User {
             std::cout << "Destructor" << std::endl;
         }
 };
-// Overloading the insertion operator
+// Overloading the << operator
 std::ostream& operator << (std::ostream& output, User user) {
     output << "First Name:" << user.firstName << "\nLast Name: " << user.lastName << "\nRank: " << user.getRank() << std::endl;
     return output;
 }
-
+// Overlading the >> operator.
 std::istream& operator >> (std::istream &input, User &user) {
     input >> user.firstName >> user.lastName;
     return input;
@@ -46,51 +46,3 @@ int main(){
 
     return 0;
 }
-
-
-
-
-/*
-#include <iostream>
-
-class Position {
-    public:
-        int x = 10;
-        int y = 20;
-        Position operator + (Position pos){ // Overloading the + operator.
-            Position newPos;
-            newPos.x = x + pos.x;           // x reffers to "pos1" (line 17), pos.x reffers to the parameter.
-            newPos.y = y + pos.y;
-            return newPos;
-        }
-        bool operator == (Position pos){    // Overloading the == operator.
-            if(x == pos.x && y == pos.y) {
-                return true;
-            } else {
-                return false;
-            }
-
-            return 0;
-        }
-};
-
-int main(){
-
-    Position pos1, pos2;
-    Position pos3 = pos1 + pos2;
-
-    std::cout << pos3.x << std::endl;
-    std::cout << pos3.y << std::endl;
-
-    if(pos1 == pos2){
-        std::cout << "They are the same!" << std::endl;
-    } else {
-        std::cout << "They are not the same!" << std::endl;
-    }
-    return 0;
-}
-
-
-*/
-
-
