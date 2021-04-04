8c4430b8e12d87ccdd07cc1fb4e46cf5f2fca606
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-04-04 19:57:51 +0200

    Teacher-Header
    Header file for the teacher object - contains function declarations.

---
+++ topics/Object Oriented Programming/Polymorphism/Teacher.h
@@ -0,0 +1,15 @@
+#ifndef TEACHER_H
+#define TEACHER_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include "User.h"
+
+class Teacher : public User {
+    public:
+        Teacher();
+        std::vector<std::string> classes;
+        void output();
+};
+#endif
